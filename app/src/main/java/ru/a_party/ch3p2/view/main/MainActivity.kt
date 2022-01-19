1ccb050ad88fa7214cb1624f54ba38aa6f9f38cb
andreysc1976 <andreysc@yandex.ru>
2022-01-19 23:22:49 +0300

    init++

---
+++ app/src/main/java/ru/a_party/ch3p2/view/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.a_party.ch3p2.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.a_party.ch3p2.R
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
